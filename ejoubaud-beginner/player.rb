ff5878e055f28a27e74978e57e60514a4b9f9f7d
ejoubaud <emmanuel.joubaud@gmail.com>
2014-07-03 16:26:44 +0200

    Level 3 done. Level 4

--- ejoubaud-beginner/player.rb
+++ ejoubaud-beginner/player.rb
@@ -1,9 +1,18 @@
 class Player
+  attr_reader :warrior
+
   def play_turn(warrior)
+    @warrior = warrior
     if warrior.feel.enemy?
       warrior.attack!
+    elsif low_health?
+      warrior.rest!
     else
       warrior.walk!
     end
+  end
+
+  def low_health?
+    warrior.health < 7
   end
 end
