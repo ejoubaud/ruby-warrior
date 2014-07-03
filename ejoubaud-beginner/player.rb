06818f7b72f1e8da9128513f6dbdfd6f2ab5839d
ejoubaud <emmanuel.joubaud@gmail.com>
2014-07-03 17:02:25 +0200

    Level 4 done. Level 5..

--- ejoubaud-beginner/player.rb
+++ ejoubaud-beginner/player.rb
@@ -3,16 +3,44 @@ class Player
 
   def play_turn(warrior)
     @warrior = warrior
-    if warrior.feel.enemy?
-      warrior.attack!
+
+    if should_charge?
+      charge!
     elsif low_health?
       warrior.rest!
     else
       warrior.walk!
     end
+
+    remember_health
   end
 
   def low_health?
-    warrior.health < 7
+    warrior.health < 12
+  end
+
+  def under_attack?
+    warrior.health < previous_health
+  end
+
+  def should_charge?
+    warrior.feel.enemy? || under_attack?
+  end
+
+  def charge!
+    if warrior.feel.enemy?
+      warrior.attack!
+    else
+      warrior.walk!
+    end
+  end
+
+  def remember_health
+    @previous_health = warrior.health
+  end
+
+  def previous_health
+    @previous_health || 0
   end
 end
+
